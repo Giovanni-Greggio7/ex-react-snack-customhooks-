29703a5f8ad962ca5a6f279b03ee1548b805f067
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-05-16 19:53:30 +0200

    Start: milestone 1

---
+++ src/assets/hooks/useSwitch.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+
+export default function useSwitch(initialValue = false){
+
+    const [ isOn, setIsOn ] = useState(initialValue)
+
+    //MIO METODO
+    // function toggle(){
+    //     if(isOn){
+    //         setIsOn(false)
+    //     } else {
+    //         setIsOn(true)
+    //     }
+    // }
+    
+    //METODO EFFICACE
+    const toggle = () => {
+        setIsOn(curr => !curr)
+    }
+
+    return [ isOn, toggle ] 
+
+}
